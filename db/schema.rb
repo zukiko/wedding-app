5b7bc3b1d266437a325a4850610901fbd49ad373
zukiko <kbtharu0508@gmail.com>
2022-04-07 11:50:57 +0900

    seating_listsテーブルにカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_04_064637) do
+ActiveRecord::Schema.define(version: 2022_04_07_024252) do
 
   create_table "seating_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_04_04_064637) do
     t.integer "seat_number", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "message"
     t.index ["user_id"], name: "index_seating_lists_on_user_id"
   end
 
