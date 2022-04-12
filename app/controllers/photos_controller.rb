ba2d4963734640b64541a924807a519ba4f5679c
zukiko <kbtharu0508@gmail.com>
2022-04-12 20:23:54 +0900

    photos#index修正

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,7 +10,7 @@ class PhotosController < ApplicationController
   def create
     @photo=Photo.new(photo_params)
     if @photo.save
-      redirect_to root_path
+      redirect_to photos_path
     else
       render :new
     end
