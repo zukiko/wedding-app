af1a8220bf6b35ab19e2c5b6356c49a499d0158d
zukiko <kbtharu0508@gmail.com>
2022-05-10 19:56:27 +0900

    写真投稿　編集ページ　実装

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -21,6 +21,19 @@ class PhotosController < ApplicationController
     end
   end
 
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update(photo_params)
+      redirect_to photos_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @photo = Photo.find(params[:id])
     @photo.destroy
